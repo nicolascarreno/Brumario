320e5af3799bb37cf8d5ff8f2a44389ff57cad4d
nicolascarreno <ncarreno@fi.uba.ar>
2025-08-14 21:48:10 -0300

    feat: Página de inicio (en progreso)

--- frontend/src/pages/inicio.tsx
+++ frontend/src/pages/inicio.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import '../styles/App.css';
 import '../styles/inicio.css';
+import escudo from '../public/brumario_escudo.jpeg';
+import diego from '../public/diego.png';
+import brumario from '../public/brumario.png';
 
 export function Inicio() {
   const navigate = useNavigate();
   return (
     <div className="App">
       <div className="encabezado">     
-        <h1>11 Brumario</h1>
+        <img src={brumario} alt="brumario" height={90} width={380} style={{ marginLeft: '18px', paddingTop: 20, paddingBottom: 20 }} />
+        <span className='sitio_web'>Sitio Web Oficial</span>
       </div>
       <div className="botones-container">
         <button className="boton-cuadrado" onClick={() => navigate('/jugadores')}>
+          <img src={diego} alt="Diego" className="boton-imagen" width={160} height={190} />
           <span>Jugadores</span>
         </button>
         <button className="boton-cuadrado">
+          <img src={escudo} alt="Escudo Brumario" className="boton-imagen" width={195} height={195} />
           <span>Partidos</span>
         </button>
-      </div>
+      </div>  
     </div>
   );
 }
+
+
+
+{/*Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif*/}
--- frontend/src/pages/jugadores.tsx
+++ frontend/src/pages/jugadores.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import '../styles/App.css';
 import '../styles/inicio.css';
+import escudo from '../public/brumario_escudo.jpeg';
+import brumario from '../public/brumario.png';
 
 export function Jugadores() {
   const navigate = useNavigate();
   return (
     <div className="App">
       <div className="encabezado">     
-        <h1>11 Brumario</h1>
+        <img src={brumario} alt="brumario" height={90} width={380} style={{ marginLeft: '18px', paddingTop: 20, paddingBottom: 20 }} />
+        <span className='sitio_web'>Sitio Web Oficial</span>
       </div>
       <h1>Jugadores</h1>
     </div>
