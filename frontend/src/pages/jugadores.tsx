a7e70068c4c3d766acda47ee8fe6f45f77a3b0da
nicolascarreno <ncarreno@fi.uba.ar>
2025-08-14 22:05:22 -0300

    fix: cambio index.html para agregar escudo y nombre

--- frontend/src/pages/jugadores.tsx
+++ frontend/src/pages/jugadores.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import '../styles/App.css';
 import '../styles/inicio.css';
-import escudo from '../public/brumario_escudo.jpeg';
-import brumario from '../public/brumario.png';
+//import brumario from '../../public/brumario.png';
 
 export function Jugadores() {
   const navigate = useNavigate();
   return (
     <div className="App">
       <div className="encabezado">     
-        <img src={brumario} alt="brumario" height={90} width={380} style={{ marginLeft: '18px', paddingTop: 20, paddingBottom: 20 }} />
+        <img src={'/brumario.png'} alt="brumario" height={90} width={380} style={{ marginLeft: '18px', paddingTop: 20, paddingBottom: 20 }} />
         <span className='sitio_web'>Sitio Web Oficial</span>
       </div>
       <h1>Jugadores</h1>
