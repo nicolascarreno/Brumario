bfae97ee21fa4e661371a95ad2f713d36bd50a72
nicolascarreno <67985054+nicolascarreno@users.noreply.github.com>
2025-10-26 13:18:13 -0300

    fix: intento deployar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import Persona from './models/persona';
 import jugadoresRouter from "./routes/jugadoresRoutes";
 import partidosRouter from "./routes/partidosRoutes";
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
@@ -16,6 +16,14 @@ app.use("/jugadores", jugadoresRouter);
 app.use("/partidos", partidosRouter);
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hola desde el backend!' });
+});
+
+// 📦 Servir el frontend buildado de React
+const frontendPath = path.join(__dirname, '../../frontend/build');
+app.use(express.static(frontendPath));
+
+app.get(/.*/, (_, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
